c267548ad97ddf7c9798810da5ef99681d6982f1
Ömer Kural <34073625+OmerKural@users.noreply.github.com>
2025-01-13 22:51:22 +0300

    1. Dersin ödevleri eklendi.

---
+++ Ders_1/kolay.py
@@ -0,0 +1,6 @@
+ad_soyad = input("Adınızı giriniz: ")
+okul_no = input("Okul Numaranızı giriniz: ")
+ders_adi = input("Ders adını giriniz: ")
+ortalama = input("Ortalamanızı giriniz: ")
+
+print(f"Okulumuz {okul_no} nolu öğrencisi {ad_soyad}, bu dönem sonunda {ders_adi} dersinden {ortalama} ortalama başarmıştır.")
---
+++ Ders_1/orta.py
@@ -0,0 +1,7 @@
+num1 = float(input("1. Sayıyı giriniz: "))
+num2 = float(input("2. Sayıyı giriniz: "))
+
+print(f"Toplam: {num1+num2}")
+print(f"Çarpım: {num1*num2}")
+print(f"Fark: {num1-num2}")
+print(f"Bölüm: {num1/num2}")
